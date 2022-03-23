fca3aa375d7b29135a534076f6f43ef159041504
ehab-fadl <56725108+ehab-fadl@users.noreply.github.com>
2022-03-23 15:43:59 +0100

    ehab-fadl
    online

--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -11,8 +11,11 @@ export class ItemsService {
   constructor(private http:HttpClient) { }
   getData()
   {
-    let url="http://192.168.0.10:8888/api/books";
+
+    let url="http://webapidownloadbooks-dev.us-west-1.elasticbeanstalk.com/api/books";
     return this.http.get(url);
   }
 }
 //THB//http://192.168.0.100:6555/api/books
+//
+//http://192.168.0.10:8888/api/books
